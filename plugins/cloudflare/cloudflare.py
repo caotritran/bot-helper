a23e872552f56960f9fd337db70d7b21b5193573
caotritran <caotritran.14@gmail.com>
2022-08-14 21:16:05 +0700

    update function

--- plugins/cloudflare/cloudflare.py
+++ plugins/cloudflare/cloudflare.py
@@ -96,6 +96,45 @@ def update_dns_recordCNAME(domain, ip):
     response = requests.put('https://api.cloudflare.com/client/v4/zones/{0}/dns_records/{1}'.format(zoneid,dns_recordid), headers=headers, json=json_data)
     return response.status_code
 
+def create_subdomain(subname, domain, ip):
+    zoneid = get_zoneid(domain)
+    headers = {
+        'X-Auth-Email': 'caotritran.14@gmail.com',
+        'X-Auth-Key': '{}'.format(X_Auth_Key),
+    
+    }
+
+    json_data = {
+        'type': 'A',
+        'name': '{}'.format(subname),
+        'content': '{}'.format(ip),
+        'ttl': 1,
+        'proxied': False,
+}
+
+    response = requests.post('https://api.cloudflare.com/client/v4/zones/{0}/dns_records'.format(zoneid), headers=headers, json=json_data)
+    return response
+
+def update_subdomain(subname, domain, ip):
+    zoneid = get_zoneid(domain)
+    dns_recordid = get_dns_recordid(domain)
+    headers = {
+        'X-Auth-Email': 'caotritran.14@gmail.com',
+        'X-Auth-Key': '{}'.format(X_Auth_Key),
+    
+    }
+
+    json_data = {
+        'type': 'A',
+        'name': '{}'.format(subname),
+        'content': '{}'.format(ip),
+        'ttl': 1,
+        'proxied': False,
+}
+
+    response = requests.put('https://api.cloudflare.com/client/v4/zones/{0}/dns_records/{1}'.format(zoneid, dns_recordid), headers=headers, json=json_data)
+    return response
+
 def get_dns_record(domain):
     zoneid = get_zoneid(domain)
 
@@ -130,7 +169,7 @@ class CLOUDFLARE(BotPlugin):
         ip = args[2].strip()
 
         if len(args) < 2 or len(args) > 3:
-            text = "`invalid syntax, _syntax: .cf updatedns <A|CNAME> <fqdn> <ip>`"
+            text = "`invalid syntax, _syntax: /cloudflare updatedns <A|CNAME> <fqdn> <ip>`"
             self._bot.send_simple_reply(msg, text, threaded=True)
             return
 
@@ -154,7 +193,7 @@ class CLOUDFLARE(BotPlugin):
                 self._bot.send_simple_reply(msg, text, threaded=True)
             return
         else:
-            text = "`invalid syntax, _syntax: .cf updatedns <A|CNAME> <fqdn> <ip>`"
+            text = "`invalid syntax, _syntax: /cloudflare updatedns <A|CNAME> <fqdn> <ip>`"
             self._bot.send_simple_reply(msg, text, threaded=True)
             return
 
@@ -192,3 +231,66 @@ class CLOUDFLARE(BotPlugin):
             text = "`somethings went wrong, status code {}`".format(data.status_code)
             self._bot.send_simple_reply(msg, text, threaded=True)
             return
+
+    @botcmd(split_args_with=None)
+    def cloudflare_create_subdomain(self, msg, args):
+        """_syntax: /cloudflare create subdomain <sub_name> <fqdn> <ip>"""
+        sub_name = args[0].lower().strip()
+        domain_name = args[1].lower().strip()
+        ip = args[2].strip()
+
+        table = []
+        headers = ["Type", "Name", "Content"]
+        text = ""
+
+        if len(args) < 3 or len(args) > 4:
+            text = "`invalid syntax, _syntax: /cloudflare create subdomain <sub_name> <fqdn> <ip>`"
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            return
+        state = create_subdomain(sub_name, domain_name, ip)
+        if state.status_code == 200:
+            text = "`create subdomain success, status code {}`".format(state.status_code)
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            data = json.loads(state.text)
+            table.append([
+                data['result'].get('type'),
+                data['result'].get('name'),
+                data['result'].get('content')
+            ])
+            text = tabulate(table, headers=headers, tablefmt="pretty")
+            self._bot.send_simple_reply(msg, text, threaded=True)
+        else:
+            text = "`something went wrong\n@tritran14 oi, check nha`"
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            
+
+    @botcmd(split_args_with=None)
+    def cloudflare_update_subdomain(self, msg, args):
+        """_syntax: /cloudflare update subdomain <sub_name> <fqdn> <ip>"""
+        sub_name = args[0].lower().strip()
+        domain_name = args[1].lower().strip()
+        ip = args[2].strip()
+
+        table = []
+        headers = ["Type", "Name", "Content"]
+        text = ""
+
+        if len(args) < 3 or len(args) > 4:
+            text = "`invalid syntax, _syntax: /cloudflare update subdomain <sub_name> <fqdn> <ip>`"
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            return
+        state = update_subdomain(sub_name, domain_name, ip)
+        if state.status_code == 200:
+            text = "`update subdomain success, status code {}`".format(state.status_code)
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            data = json.loads(state.text)
+            table.append([
+                data['result'].get('type'),
+                data['result'].get('name'),
+                data['result'].get('content')
+            ])
+            text = tabulate(table, headers=headers, tablefmt="pretty")
+            self._bot.send_simple_reply(msg, text, threaded=True)
+        else:
+            text = "`something went wrong\n@tritran14 oi, check nha`"
+            self._bot.send_simple_reply(msg, text, threaded=True)
--- plugins/jenkins/jenkins.py
+++ plugins/jenkins/jenkins.py
@@ -1,3 +1,4 @@
+from ast import arg
 import time
 from errbot import BotPlugin, botcmd
 from sys import exit
@@ -16,6 +17,11 @@ class JENKINS(BotPlugin):
     
     @botcmd(split_args_with=None)
     def jenkins_findrootip_offshore(self, msg, args):
+        """_syntax: /jenkins findrootip offshore <ip>"""
+        if len(args) < 1 or len(args) > 1:
+            text = "`invalid syntax, _syntax:  /jenkins findrootip offshore <ip>`"
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            return
         ip_offshore = args[0]
         URL = "http://jenkins.sweb.vn/job/sweb/job/Check_IP_From_Offshore"
 
@@ -42,6 +48,48 @@ class JENKINS(BotPlugin):
             text = "Send trigger build to jenkins fail\n @tritran14 oi vao check ne` !!!"
             self._bot.send_simple_reply(msg, text, threaded=True)
             time.sleep(40)
+            console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+            output_text = console_output.text
+            self._bot.send_simple_reply(msg, output_text, threaded=True)
+
+
+    @botcmd(split_args_with=None)
+    def jenkins_coverip_offshore(self, msg, args):
+        """_syntax: /jenkins coverip offshore <domain_name> <root_ip> <offshore_ip>"""
+        if len(args) < 3 or len(args) > 3:
+            text = "`invalid syntax, _syntax: /jenkins coverip offshore <domain_name> <root_ip>`"
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            return
+
+        domain_name = args[0]
+        root_ip = args[1]
+        offshore_ip = args[2]
+
+        URL = "http://jenkins.sweb.vn/job/sweb/job/Offshore_Cover/"
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+
+        data = 'json={"parameter": [{"name":"HOSTS", "value":"%s"}, {"name":"Domain", "value":"%s"}, {"name":"RootIP", "value":"%s"}]}' % (offshore_ip, domain_name, root_ip)
+
+        response = requests.post(URL+"/build", headers=headers, data=data, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+
+        output_url = URL + "/lastBuild/consoleText"
+
+        if response.status_code == 201:
+            text = "Send trigger build to jenkins success\nGenarating output - please wait ..."
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            time.sleep(150)
+            console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+            output_text = console_output.text
+            self._bot.send_simple_reply(msg, output_text, threaded=True)
+            
+
+        else:
+            text = "Send trigger build to jenkins fail\n @tritran14 oi vao check ne` !!!"
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            time.sleep(150)
             console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
             output_text = console_output.text
             self._bot.send_simple_reply(msg, output_text, threaded=True)
