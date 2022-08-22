0322ab25daa22833fa1b66c0a0b9bf0b4c3958d6
caotritran <caotritran.14@gmail.com>
2022-08-22 15:25:01 +0700

    update cmd create ssl

--- plugins/jenkins/jenkins.py
+++ plugins/jenkins/jenkins.py
@@ -93,3 +93,44 @@ class JENKINS(BotPlugin):
             console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
             output_text = console_output.text
             self._bot.send_simple_reply(msg, output_text, threaded=True)
+
+
+    @botcmd(split_args_with=None)
+    def jenkins_createssl(self, msg, args):
+        """_syntax: /jenkins createssl <root_ip> <domain_name>"""
+        if len(args) < 3 or len(args) > 3:
+            text = "`invalid syntax, _syntax: /jenkins createssl <root_ip> <domain_name>`"
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            return
+
+        root_ip = args[0]
+        domain_name = args[1]
+
+        URL = "http://jenkins.sweb.vn/job/sweb/job/Create_SSL/"
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+
+        data = 'json={"parameter": [{"name":"HOSTS", "value":"%s"}, {"name":"Domain", "value":"%s"}' % (root_ip, domain_name)
+
+        response = requests.post(URL+"/build", headers=headers, data=data, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+
+        output_url = URL + "/lastBuild/consoleText"
+
+        if response.status_code == 201:
+            text = "Send trigger build to jenkins success\nGenarating output - please wait ..."
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            time.sleep(50)
+            console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+            output_text = console_output.text
+            self._bot.send_simple_reply(msg, output_text, threaded=True)
+            
+
+        else:
+            text = "Send trigger build to jenkins fail\n @tritran14 oi vao check ne` !!!"
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            time.sleep(50)
+            console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+            output_text = console_output.text
+            self._bot.send_simple_reply(msg, output_text, threaded=True)
