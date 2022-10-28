f0e161c8a826b98ee4aa86f6fa2b420371bd680d
caotritran <caotritran.14@gmail.com>
2022-10-28 22:34:35 +0700

    update new comment backup restore

--- plugins/jenkins/jenkins.py
+++ plugins/jenkins/jenkins.py
@@ -6,6 +6,7 @@ import requests, json, urllib3, os, re
 from tabulate import tabulate
 
 from dotenv import load_dotenv
+from plugins.cloudflare.cloudflare import update_dns_recordA
 load_dotenv('.env')
 
 JENKINS_API_TOKEN = os.environ['JENKINS_API_TOKEN']
@@ -331,3 +332,124 @@ class JENKINS(BotPlugin):
         else:
             text = "Send trigger build to jenkins fail\n @tritran14 oi vao check ne` {}!!!".format(response.status_code)
             self._bot.send_simple_reply(msg, text, threaded=True)
+
+    @botcmd(split_args_with=None)
+    def jenkins_backup_restore(self, msg, args):
+        """_syntax: /jenkins backup restore <source_ip> <dest_ip> <domain>"""
+        if len(args) != 3:
+            text = "`invalid syntax, _syntax: /jenkins backup restore <source_ip> <dest_ip> <domain>`"
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            return
+
+        source_ip = args[0]
+        dest_ip = args[1]
+        domain = args[2]
+        backup_url = "http://jenkins.sweb.vn/job/sweb/job/Backup_website/"
+        create_vhost_url = "http://jenkins.sweb.vn/job/sweb/job/Create_vhost/"
+        restore_url = "http://jenkins.sweb.vn/job/sweb/job/Restore_website/"
+        ssl_url = "http://jenkins.sweb.vn/job/sweb/job/Create_SSL/"
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+
+        data_backup = 'json={"parameter": [{"name":"HOSTS", "value":"%s"}, {"name":"Domain", "value":"%s"}]}' % (source_ip, domain)
+
+        response = requests.post(backup_url+"/build", headers=headers, data=data_backup, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+
+        output_url = backup_url + "/lastBuild/consoleText"
+        if response.status_code == 201:
+            text = "Send trigger backup to jenkins success - please wait ..."
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            time.sleep(330)
+            console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+            output_text = console_output.text
+
+            for line in output_text.splitlines():
+                if re.search("backup was completed", line):
+                    backup_link = line[60:-1]
+                    text = "Success backup - download manual here: {} - continue restore ...".format(backup_link)
+                    self._bot.send_simple_reply(msg, text, threaded=True)
+                else:
+                    text = "Build fail roi @tritran14 oi!!!"
+                    self._bot.send_simple_reply(msg, text, threaded=True)
+                    break
+
+        data_create_vhost = 'json={"parameter": [{"name":"HOSTS", "value":"%s"}, {"name":"Domain", "value":"%s"}]}' % (dest_ip, domain)
+
+        response = requests.post(create_vhost_url+"/build", headers=headers, data=data_create_vhost, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+
+        output_url = create_vhost_url + "/lastBuild/consoleText"
+
+        if response.status_code == 201:
+            text = "Creating vhost - please wait ..."
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            time.sleep(60)
+            console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+            output_text = console_output.text
+            if re.search("SUCCESS", output_text):
+                text = "Create vhost completed - continue restore website ..."
+                self._bot.send_simple_reply(msg, text, threaded=True)
+            else:
+                text = "Build fail roi @tritran14 oi!!!"
+                self._bot.send_simple_reply(msg, text, threaded=True)
+                return
+
+        data_restore = 'json={"parameter": [{"name":"HOSTS", "value":"%s"}, {"name":"Domain", "value":"%s"}, {"name":"SOURCE_URL", "value":"%s"}]}' % (dest_ip, domain, backup_link)
+
+        response = requests.post(backup_url+"/build", headers=headers, data=data_restore, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+
+        output_url = restore_url + "/lastBuild/consoleText"
+        if response.status_code == 201:
+            text = "Send trigger restore to jenkins success - please wait ..."
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            time.sleep(330)
+            console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+            output_text = console_output.text
+
+            
+            if re.search("SUCCESS", output_text):
+                text = "restore was completed - continue point dns to destination IP..."
+                self._bot.send_simple_reply(msg, text, threaded=True)
+            else:
+                text = "Build fail roi @tritran14 oi!!!"
+                self._bot.send_simple_reply(msg, text, threaded=True)
+                return
+
+        state = update_dns_recordA(domain,dest_ip)
+        if state == 200:
+            text = "update record success, status code {} - continue issue SSL LE...".format(state)
+            self._bot.send_simple_reply(msg, text, threaded=True)
+        else:
+            text = "somethings went wrong, status code {}".format(state)
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            return
+
+        #SSL
+        data_ssl = 'json={"parameter": [{"name":"HOSTS", "value":"%s"}, {"name":"Domain", "value":"%s"}]}' % (dest_ip, domain, backup_link)
+
+        response = requests.post(backup_url+"/build", headers=headers, data=data_ssl, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+
+        output_url = restore_url + "/lastBuild/consoleText"
+        if response.status_code == 201:
+            text = "Send trigger issue SSL to jenkins success - please wait ..."
+            self._bot.send_simple_reply(msg, text, threaded=True)
+            time.sleep(60)
+            console_output = requests.get(output_url, auth=('admin', '{}'.format(JENKINS_API_TOKEN)))
+            output_text = console_output.text
+
+            
+            if re.search("SUCCESS", output_text):
+                text = "Create SSL completed - restore step completed!"
+                self._bot.send_simple_reply(msg, text, threaded=True)
+            else:
+                text = "Build fail roi @tritran14 oi!!!"
+                self._bot.send_simple_reply(msg, text, threaded=True)
+                return
+
+        
+
+
+
+
+
