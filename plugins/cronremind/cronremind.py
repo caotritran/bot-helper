a4dc1139083ed6d50b5a15201eafec1b77d33486
caotritran <caotritran.14@gmail.com>
2022-11-02 23:41:47 +0700

    edit code

--- plugins/cronremind/cronremind.py
+++ plugins/cronremind/cronremind.py
@@ -8,16 +8,15 @@ class ErrcronDemo(CrontabMixin, BotPlugin):
     Remind task daily
     """
     CRONTAB = [
-        '* * * * * .remind_daily @pikabot',
+        '0 8 * * * .remind_daily @pikabot',
     ]
 
     def activate(self):
         super().activate()
 
     def remind_daily(self, polled_time, identity):
-        #identity = "1019630113"
         identity = TELE_GROUP_ID
         user = self.build_identifier(identity)
-        return self.send(user, 'Currently {}'.format(polled_time.strftime('%H:%M')))
-        #text = "***Remind***\n" + "1. What are the issues today? - Check tele [Alert] 🤖 Sweb\n" +  "2. What are the complaints or renew payment today? - Check the email\n" + "cc @tritran14"
-        #return self.send(user, text)
+        #return self.send(user, 'Currently {}'.format(polled_time.strftime('%H:%M')))
+        text = "***Remind***\n" + "1. What are the issues today? - Check tele [Alert] 🤖 Sweb\n" +  "2. What are the complaints or renew payments today? - Check the email\n" + "cc @tritran14 @Cuong"
+        return self.send(user, text)
