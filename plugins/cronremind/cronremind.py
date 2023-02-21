ce83bd4a1cf1def0c28b9be43f46fdf1dc6dc96c
tritran <tri@emobi.biz>
2023-02-21 17:31:53 +0700

    update remind onedrive

--- plugins/cronremind/cronremind.py
+++ plugins/cronremind/cronremind.py
@@ -9,6 +9,7 @@ class ErrcronDemo(CrontabMixin, BotPlugin):
     """
     CRONTAB = [
         '0 8 * * * .remind_daily @pikabot',
+        '10 8 * * 6 .remind_weekly @pikabot',
     ]
 
     def activate(self):
@@ -19,4 +20,11 @@ class ErrcronDemo(CrontabMixin, BotPlugin):
         user = self.build_identifier(identity)
         #return self.send(user, 'Currently {}'.format(polled_time.strftime('%H:%M')))
         text = "***Remind***\n" + "1. What are the issues today? - Check tele [Alert] 🤖 Sweb\n" +  "2. What are the complaints or renew payments today? - Check the email\n" + "cc @tritran14 @Cuong"
+        return self.send(user, text)
+
+    def remind_weekly(self, polled_time, identity):
+        identity = TELE_GROUP_ID
+        user = self.build_identifier(identity)
+        #return self.send(user, 'Currently {}'.format(polled_time.strftime('%H:%M')))
+        text = "***Remind***\n" + "Please check and clear history trash Onedrive !!!\n" + "cc @tritran14"
         return self.send(user, text)
