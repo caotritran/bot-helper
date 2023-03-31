6872b55ce82f55a3dce45227d3611808119817d4
tritran <tri@emobi.biz>
2023-03-31 17:05:09 +0700

    update remind check domain point ip AWS

--- plugins/cronremind/cronremind.py
+++ plugins/cronremind/cronremind.py
@@ -1,6 +1,6 @@
 from errbot import BotPlugin
 from errcron import CrontabMixin
-import os
+import os, subprocess
 
 TELE_GROUP_ID = os.environ['TELE_GROUP_ID']
 class ErrcronDemo(CrontabMixin, BotPlugin):
@@ -10,6 +10,7 @@ class ErrcronDemo(CrontabMixin, BotPlugin):
     CRONTAB = [
         '0 8 * * * .remind_daily @pikabot',
         '10 8 * * 6 .remind_weekly @pikabot',
+        '0 19 * * * .remind_daily_ippoint @pikabot',
     ]
 
     def activate(self):
@@ -28,3 +29,13 @@ class ErrcronDemo(CrontabMixin, BotPlugin):
         #return self.send(user, 'Currently {}'.format(polled_time.strftime('%H:%M')))
         text = "***Remind***\n" + "Please check and clear history trash Onedrive !!!\n" + "cc @tritran14"
         return self.send(user, text)
+    
+    def remind_daily_ippoint(self, polled_time, identity):
+        identity = TELE_GROUP_ID
+        user = self.build_identifier(identity)
+        plugin_path = os.path.dirname(os.path.realpath(__file__))
+        other_file_path = os.path.join(plugin_path, "checkippoint.py")
+        output = subprocess.check_output(["/opt/bot-helper/venv/bin/python3.8", other_file_path]).decode("utf-8")
+
+        # send output as message
+        return self.send(user, output)
